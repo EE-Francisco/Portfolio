3b985979e85b41f39e90a4b139aac6619b4658cd
Francisco <2420171077@estudiantesunibague.edu.co>
2023-02-15 21:53:34 -0500

    env

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -9,9 +9,9 @@ environ.Env.read_env()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
 
-DEBUG = os.environ.get('DEBUG')
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
