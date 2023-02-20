91d4018286092865f9be58b85a711adb6afcf0ab
Francisco <2420171077@estudiantesunibague.edu.co>
2023-02-20 00:01:55 -0500

    cv view

--- apps/portfolio_d/views.py
+++ apps/portfolio_d/views.py
@@ -5,3 +5,7 @@ from .models import Project
 def home(request):
     projects = Project.objects.all()
     return render(request, "home.html", {"projects": projects})
+
+
+def cv(request):
+    return render(request, "cv.html")
