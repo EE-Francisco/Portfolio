49885ce3268e58a8468ea64a74c2ba653c3e5655
Francisco <2420171077@estudiantesunibague.edu.co>
2023-04-25 18:23:06 -0500

    AuthenticationForm

---
+++ apps/user/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UsernameField
+
+
+class CustomAuthenticationForm(AuthenticationForm):
+    username = UsernameField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
